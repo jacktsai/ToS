299cbc9a39099bacb82dcdf6be044c35e2ef07a3
Jack Tsai <wa0327@gmail.com>
2014-06-28 04:17:17 +0800

    make a bit changes

--- 6.05/Assembly-Hijack/src/Assembly-Hijack/Automation/MergeCard.cs
+++ 6.05/Assembly-Hijack/src/Assembly-Hijack/Automation/MergeCard.cs
@@ -8,10 +8,30 @@ namespace AssemblyHijack.Automation
 {
     internal class MergeCard : Runnable
     {
-        private static int[] ExpCard = new[] { 280, 281, 282, 283, 284, 291, 398, 426, 427, 428 };
-        private static int Up1LevelCard = 423; // 異域法眼，強化時必定升上1個等級
-        private static int Up3LevelCard = 424; // 機關騎士，強化時必定升上3個等級
-        private static int Up5LevelCard = 425; // 永劫碑文像，強化時必定升上5個等級
+        /// <summary>
+        /// 經驗值卡
+        /// </summary>
+        private static int[] ExpCards = new[] { 280, 281, 282, 283, 284, 291, 398, 426, 427, 428 };
+
+        /// <summary>
+        /// 異域法眼，強化時必定升上1個等級
+        /// </summary>
+        private static int Up1LevelCard = 423;
+
+        /// <summary>
+        /// 機關騎士，強化時必定升上3個等級
+        /// </summary>
+        private static int Up3LevelCard = 424;
+
+        /// <summary>
+        /// 永劫碑文像，強化時必定升上5個等級
+        /// </summary>
+        private static int Up5LevelCard = 425;
+
+        /// <summary>
+        /// 等級卡
+        /// </summary>
+        private static int[] LevelCards = new[] { Up1LevelCard, Up3LevelCard, Up5LevelCard };
 
         private class UpgradeInfo
         {
@@ -24,9 +44,9 @@ namespace AssemblyHijack.Automation
 
         private UpgradeInfo upgradeInfo = null;
 
-        private Card target = null;
-        private IList<Card> children = new List<Card>();
-        private int expectedCost = 0;
+        private Card target;
+        private List<Card> children = new List<Card>();
+        private int expectedCost;
 
         public override void AppendReport(StringBuilder builder)
         {
@@ -43,52 +63,53 @@ namespace AssemblyHijack.Automation
 
         protected override bool Check()
         {
-            var sacrificers = Game.runtimeData.user.inventory.cards.Values
-                .Where(c => !c.inUse && !c.bookmark)
-                .Where(c => ExpCard.Contains(c.monsterId))
-                .OrderBy(c => c.mergeExp)
-                .OrderBy(c => c.element)
-                .ToArray();
+            target = Game.runtimeData.user.helperCard;
 
-            if (sacrificers.Length < 1)
+            if (target.isLevelMax)
             {
-                MyLog.Debug("找不到合適的飼料卡進行強化！");
+                MyLog.Debug("{0} 已經到達最高級別 {1} 不需要再強化", target.name, target.level);
                 return false;
             }
 
-            var candidate = Game.runtimeData.user.helperCard;
-            if (candidate.isLevelMax)
+            Game.SetMonsterUpgradeTarget(target);
+
+            if (EstimateWithExpCards())
             {
-                MyLog.Debug("{0} 已經到達最高級別 {1} 不需要再強化", candidate.name, candidate.level);
+                if (EstimateCost())
+                {
+                    StringBuilder cardNames = new StringBuilder();
+
+                    children.ForEach(card =>
+                    {
+                        if (cardNames.Length > 0)
+                            cardNames.Append(",");
+                        cardNames.AppendFormat("{0}", card.name);
+                    });
+
+                    MyLog.Info("判定強化 {0} 預估費用 {1:#,0} 餵食 {2} 張卡 {3}", target.name, expectedCost, children.Count, cardNames);
+                    return true;
+                }
+                else
+                {
+                    MyLog.Debug("強化 {0} 預估費用 {1:#,0} 資產不足", target.name, expectedCost);
+                    return false;
+                }
             }
             else
             {
-                if (Estimate(candidate, sacrificers))
-                    return true;
+                MyLog.Debug("背包未滿, 暫不執行卡片強化");
+                return false;
             }
-
-            return false;
         }
 
         protected override void Execute(Action next)
         {
-            StringBuilder cardNames = new StringBuilder();
-            foreach (var card in children)
-            {
-                if (cardNames.Length > 0)
-                    cardNames.Append("\n");
-
-                cardNames.AppendFormat("{0}", card.name);
-            }
-
-            Game.SetMonsterUpgradeTarget(target);
-            Game.SetMonsterUpgradeChildren(children.ToArray());
-
             var levelBefore = target.level;
             var expBefore = target.exp;
             var coinBefore = Game.runtimeData.user.coin;
 
             MyDialog.SetNetworkWaitingText(null, "強化\n<color=yellow>{0}</color>", target.name);
+            Game.SetMonsterUpgradeChildren(children.ToArray());
             Game.UpgradeMonster(delegate
             {
                 var upgradedCard = Game.runtimeData.user.inventory.GetCard(target.cardId);
@@ -110,50 +131,67 @@ namespace AssemblyHijack.Automation
             });
         }
 
-        private bool Estimate(Card candidate, IEnumerable<Card> sarcrificers)
+        private bool EstimateWithExpCards()
         {
-            children.Clear();
-            StringBuilder cardNames = new StringBuilder();
+            var expCards = Game.runtimeData.user.inventory.cards.Values
+                .Where(c => !c.inUse && !c.bookmark)
+                .Where(c => ExpCards.Contains(c.monsterId))
+                .OrderByDescending(c => c.predictedMergeExp)
+                .ToArray();
 
-            Game.SetMonsterUpgradeTarget(candidate);
-            int requiredExp = candidate.LevelToExp(candidate.maxLevel + 1) - candidate.accumulativeLevelExp;
+            children.Clear();
+
+            int requiredExp = target.LevelToExp(target.maxLevel + 1) - target.accumulativeLevelExp;
             int expectedExp = 0;
-            int childrenBonus = 0;
-            foreach (var card in sarcrificers)
+
+            foreach (var card in expCards)
             {
                 expectedExp += card.predictedMergeExp;
-                childrenBonus += card.bonus;
                 children.Add(card);
-
-                if (cardNames.Length > 0)
-                    cardNames.Append(",");
-                cardNames.AppendFormat("{0}", card.name);
 
                 if (expectedExp >= requiredExp)
                     break;
             }
 
-            expectedCost = candidate.mergeCoin * children.Count + (candidate.bonus + childrenBonus) * Core.Config.UPGRADE_CARDBOUNS_COST;
+            return expectedExp >= requiredExp || Game.runtimeData.user.inventory.isFull;
+        }
 
-            if (Game.runtimeData.user.coin >= expectedCost)
+        private bool EstimateWithLevelCard()
+        {
+            var levelCards = Game.runtimeData.user.inventory.cards.Values
+                .Where(c => !c.inUse && !c.bookmark)
+                .Where(c => LevelCards.Contains(c.monsterId))
+                .OrderByDescending(c => c.monsterId)
+                .ToArray();
+
+            children.Clear();
+
+            int requiredLevel = target.maxLevel - target.level;
+            int expectedLevel = 0;
+            foreach (var card in levelCards)
             {
-                if (expectedExp >= requiredExp || Game.runtimeData.user.inventory.isFull)
-                {
-                    MyLog.Info("判定強化 {0} 預估經驗值 {1:#,0} 預估費用 {2:#,0} 餵食 {3} 張卡 {4}", candidate.name, expectedExp, expectedCost, children.Count, cardNames);
-                    target = candidate;
-                    return true;
-                }
-                else
-                {
-                    MyLog.Debug("背包未滿, 執行卡片強化");
-                    return false;
-                }
+                if (card.monsterId == Up1LevelCard)
+                    expectedLevel += 1;
+                else if (card.monsterId == Up3LevelCard)
+                    expectedLevel += 3;
+                else if (card.monsterId == Up5LevelCard)
+                    expectedLevel += 5;
+
+                children.Add(card);
+
+                if (expectedLevel >= requiredLevel)
+                    break;
             }
-            else
-            {
-                MyLog.Info("強化 {0} 預估需要 {1:#,0} 資產不足", candidate.name, expectedCost);
-                return false;
-            }
+
+            return children.Count > 0 || Game.runtimeData.user.inventory.isFull;
+        }
+
+        private bool EstimateCost()
+        {
+            var childrenBonus = children.Sum(card => card.bonus);
+            expectedCost = target.mergeCoin * children.Count + (target.bonus + childrenBonus) * Core.Config.UPGRADE_CARDBOUNS_COST;
+
+            return Game.runtimeData.user.coin >= expectedCost;
         }
     }
 }
